7028a9fa15d0c3f5dc728294ad2e6a96ca57e06b
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2023-11-21 04:06:36 +0200

    modal

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -0,0 +1,44 @@
+import { Component } from 'react';
+import css from './ImageGalleryItem.module.css';
+import { ModalWindow } from 'components/Modal/Modal';
+
+export class CardPhoto extends Component {
+  state = {
+    isOpenModal: false,
+  };
+
+  openModal = () => {
+    this.setState({ isOpenModal: true });
+    console.log(`OPEN`);
+  };
+  closeModal = () => {
+    this.setState({ isOpenModal: false });
+    console.log(`CLOSE`);
+  };
+
+  render() {
+    const { isOpenModal } = this.state;
+    const { openModal, closeModal } = this;
+    const {
+      photo: { tags, webformatURL },
+    } = this.props;
+
+    return (
+      <>
+        <li className={css.ImageGalleryItem} onClick={openModal}>
+          <img
+            className={css['ImageGalleryItem-image']}
+            src={webformatURL}
+            alt={tags}
+            width="200"
+          />
+        </li>
+        <ModalWindow
+          onClose={closeModal}
+          photo={this.props.photo}
+          isOpen={isOpenModal}
+        />
+      </>
+    );
+  }
+}
